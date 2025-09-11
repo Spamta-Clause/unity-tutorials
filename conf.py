40b97f0a0be435fd2cd5daa5d43c7f49644843be
Spamta <SamNickDM@gmail.com>
2025-09-11 21:11:54 +1000

    Config

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+source_suffix=['.rst','.md']
 
 
 # -- Options for HTML output -------------------------------------------------
